a9e739c1daa5f1d48a0a5d9cc67821d9335aeff1
Christopher Harrington <ironiridis@gmail.com>
2015-08-04 13:48:46 -0500

    Don't make a backup file at all

--- cal.sh
+++ cal.sh
@@ -16,7 +16,7 @@ sweep() {
   let "N %= 255"
   echo looking for open port 23 in $MYSUBNET.$N.0/24
   nmap -p 23 --open -oG hosts_$MYSUBNET.$N $MYSUBNET.$N.0/24
-  sed -En -i X 's/^Host: ([0-9.]+) .+Ports:.+$/\1/p' hosts_$MYSUBNET.$N
+  sed -En -i 's/^Host: ([0-9.]+) .+Ports:.+$/\1/p' hosts_$MYSUBNET.$N
   for H in `cat hosts_$MYSUBNET.$N`
   do connect $H
   done

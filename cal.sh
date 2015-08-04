001c44a9ce698ef28a5f602f84a1b34364afa7aa
Christopher Harrington <ironiridis@gmail.com>
2015-08-04 13:38:53 -0500

    Update cal.sh

--- cal.sh
+++ cal.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-cd
-mkdir -p cal
-cd cal
+# cd
+# mkdir -p cal
+# cd cal
+
+connect() {
+  echo would connect to $1
+  
+}
 
 sweep() {
   N=$RANDOM
   let "N %= 255"
   nmap -p 23 --open -oG hosts_$N 10.5.$N.0/24
-  sed -En -iX 's/^Host: ([0-9.]+) .+Ports:.+$/\1/p'
-  
-  
+  sed -En -i X 's/^Host: ([0-9.]+) .+Ports:.+$/\1/p' hosts_$N
+  for H in `cat hosts_$N`
+  do connect $H
+  done
 }
 
-sweep
+while true
+do sweep ; sleep 10 ; done

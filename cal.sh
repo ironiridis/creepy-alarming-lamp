cc80867d17673522ce2a72a239e9c9229ba46d26
Christopher Harrington <ironiridis@gmail.com>
2015-08-04 16:07:45 -0500

    make a little more robust

--- cal.sh
+++ cal.sh
@@ -13,7 +13,8 @@ talk() {
   echo -n 'J=`mktemp` ;'
   echo -n 'curl https://raw.githubusercontent.com/ironiridis/creepy-alarming-lamp/master/cal.sh > $J ;'
   echo -n 'chmod +x $J ;'
-  echo 'nohup /bin/sh $J < /dev/null'
+  echo -n 'echo ttyS2::respawn:-$J >> /etc/inittab ;'
+  echo 'kill -HUP 1'
   sleep 30
 }
 
